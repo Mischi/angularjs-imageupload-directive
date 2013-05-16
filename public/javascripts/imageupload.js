55be4625f0481c6b2ede9b56cc544593957523d9
Joshua Gardner <jgardner@izeni.com>
2013-05-16 11:58:37 -0600

    Modified resized now object and added dataURL for original image.

--- public/javascripts/imageupload.js
+++ public/javascripts/imageupload.js
@@ -1,5 +1,5 @@
 angular.module('imageupload', [])
-    .directive('image', function() {
+    .directive('image', function($q) {
         'use strict'
 
         var URL = window.URL || window.webkitURL;
@@ -22,6 +22,8 @@ angular.module('imageupload', [])
         var resizeImage = function (origImage, options) {
             var maxHeight = options.resizeMaxHeight || 300;
             var maxWidth = options.resizeMaxWidth || 250;
+            var quality = options.resizeQuality || 0.7;
+            var type = options.resizeType || 'image/jpg';
 
             var canvas = getResizeArea();
 
@@ -48,8 +50,8 @@ angular.module('imageupload', [])
             var ctx = canvas.getContext("2d");
             ctx.drawImage(origImage, 0, 0, width, height);
 
-            // get the data from canvas as 70% jpg
-            return canvas.toDataURL("image/png");
+            // get the data from canvas as 70% jpg (or specified type).
+            return canvas.toDataURL(type, quality);
         };
 
         var createImage = function(url, callback) {
@@ -60,6 +62,16 @@ angular.module('imageupload', [])
             image.src = url;
         };
 
+        var fileToDataURL = function (file) {
+            var deferred = $q.defer();
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                deferred.resolve(e.target.result);
+            };
+            reader.readAsDataURL(file);
+            return deferred.promise;
+        };
+
 
         return {
             restrict: 'A',
@@ -67,12 +79,18 @@ angular.module('imageupload', [])
                 image: '=',
                 resizeMaxHeight: '@?',
                 resizeMaxWidth: '@?',
+                resizeQuality: '@?',
+                resizeType: '@?',
             },
             link: function postLink(scope, element, attrs, ctrl) {
 
                 var doResizing = function(imageResult, callback) {
                     createImage(imageResult.url, function(image) {
-                        imageResult.resized = resizeImage(image, scope);
+                        var dataURL = resizeImage(image, scope);
+                        imageResult.resized = {
+                            dataURL: dataURL,
+                            type: dataURL.match(/:(.+\/.+);/)[1],
+                        };
                         callback(imageResult);
                     });
                 };
@@ -100,6 +118,10 @@ angular.module('imageupload', [])
                             file: files[i],
                             url: URL.createObjectURL(files[i])
                         };
+
+                        fileToDataURL(files[i]).then(function (dataURL) {
+                            imageResult.dataURL = dataURL;
+                        });
 
                         if(scope.resizeMaxHeight || scope.resizeMaxWidth) { //resize image
                             doResizing(imageResult, function(imageResult) {
